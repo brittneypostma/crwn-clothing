1134f8d2a68f82b0b5737f2de0e36f0e4415eb99
Brittney Postma <brittney.postma@hotmail.com>
2019-07-06 13:19:52 -0400

    styled signed in page

--- src/pages/SignInPage/SignInPage.component.jsx
+++ src/pages/SignInPage/SignInPage.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/SignIn/SignIn.component";
+import SignUp from "../../components/SignUp/SignUp.component";
 
 import "./SignInPage.styles.scss";
 
 const SignInPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
