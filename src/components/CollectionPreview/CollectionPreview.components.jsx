17b031a3d3bba94e9018af712867e7a3db1fb903
Brittney Postma <brittney.postma@hotmail.com>
2019-07-05 22:09:40 -0400

    updating app with shop page, collection preview component and collection item

---
+++ src/components/CollectionPreview/CollectionPreview.components.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./CollectionPreview.styles.scss";
+import CollectionItem from "../CollectionItem/CollectionItem.components";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
--- src/components/MenuItem/MenuItem.component.jsx
+++ src/components/MenuItem/MenuItem.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './MenuItem.styles.scss';
 
-export const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
 	return (
-		<div className={`${size} menu-item`}>
+		<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
 			<div
 				className="background-image"
 				style={{
@@ -17,3 +18,5 @@ export const MenuItem = ({ title, imageUrl, size }) => {
 		</div>
 	);
 };
+
+export default withRouter(MenuItem);
