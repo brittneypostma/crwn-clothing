c62205fd357c92ff1131e9f3242d4496f1be875d
Brittney Postma <brittney.postma@hotmail.com>
2019-07-09 18:58:49 -0400

    changed shop data to obj and made collection page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -23,7 +23,7 @@ const CollectionItem = ({ item, addItem }) => {
       </div>
       <div className="collection-footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
     </div>
   );
