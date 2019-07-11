c6b813010cea72895af137e14e722b88c09e3cb5
Brittney Postma <brittney.postma@hotmail.com>
2019-07-11 18:19:17 -0400

    updated HomePage link

--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import Directory from '../../components/directory/directory.component';
+import Directory from "../../components/directory/directory.component";
 
-import './homepage.styles.scss';
+import "./HomePage.styles.scss";
 
 const HomePage = () => (
-  <div className='homepage'>
+  <div className="homepage">
     <Directory />
   </div>
 );
