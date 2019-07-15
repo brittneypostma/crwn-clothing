bee7810e1c28ad9c2016d5c7fc6b570412af5def
Brittney Postma <brittney.postma@hotmail.com>
2019-07-15 19:08:30 -0400

    total location on checkout page

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -32,13 +32,13 @@ const CheckoutPage = ({ cartItems, total }) => (
 		{cartItems.map(cartItem => (
 			<CheckoutItem key={cartItem.id} cartItem={cartItem} />
 		))}
+		<div className="total">TOTAL: ${total}</div>
 		<div className="test-warning">
 			*Please use the following test credit card for payments*
 			<br />
 			4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
 		</div>
 		<StripeCheckoutButton price={total} />
-		<div className="total">TOTAL: ${total}</div>
 	</div>
 );
 
